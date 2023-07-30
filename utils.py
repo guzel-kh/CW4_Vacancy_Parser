bf0f2b69df1d45ec5c11b815d055bcef75856f13
guzel <guzel.kh23@gmail.ru>
2023-07-30 12:19:58 +0300

    func get_currencies() added

---
+++ utils.py
@@ -0,0 +1,18 @@
+import json
+import os
+from pprint import pprint
+
+import requests
+
+
+def get_currencies(currency: str='RUB') -> dict:
+    """
+    Функция для получения курсов валют к рублю
+    :param currency: название валюты курс к которому нужно получить
+    :return: курсы доступных валют к рублю.
+    """
+    API_KEY = os.getenv('EXCHANGE_RATE_API_KEY')
+    url = f"https://api.apilayer.com/exchangerates_data/latest?base={currency}"
+    response = requests.get(url, headers={'apikey': API_KEY})
+    currencies = json.loads(response.text)
+    return currencies['rates']
